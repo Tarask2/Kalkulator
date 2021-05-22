91532954963afdcd8b83dffd33b123483e941aaf
Taras Kushnir <taras.kushnir.tk@gmail.com>
2021-05-23 00:11:47 +0200

    dodano klase Kalk

---
+++ src/main/java/com/homeWork/Kalkulator/Kalk.java
@@ -0,0 +1,24 @@
+package com.homeWork.Kalkulator;
+
+public class Kalk {
+
+    int min (int x, int y){
+        if (x<y) {
+            return x;
+        }else
+            return y;
+        }
+    int max (int x, int y){
+        if (x>y) {
+            return x;
+        }else
+            return y;
+    }
+   boolean isPositive (int x){
+        if (x<0) {
+            return false;
+        }else
+            return true;
+    }
+    }
+
