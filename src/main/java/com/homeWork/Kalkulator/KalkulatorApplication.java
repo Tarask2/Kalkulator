3ff8974988da7cae4efa3d2c0b5097dc2c9d34c7
Taras Kushnir <taras.kushnir.tk@gmail.com>
2021-05-23 00:41:45 +0200

    dopisana klasa KalkulatorApplication

--- src/main/java/com/homeWork/Kalkulator/KalkulatorApplication.java
+++ src/main/java/com/homeWork/Kalkulator/KalkulatorApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KalkulatorApplication {
 
 	public static void main(String[] args) {
+		Kalk kalk = new Kalk();
+		System.out.println(kalk.min(5,6));
+		System.out.println(kalk.max(5,6));
+		System.out.println(kalk.isPositive(-1));
 
 	}
 
