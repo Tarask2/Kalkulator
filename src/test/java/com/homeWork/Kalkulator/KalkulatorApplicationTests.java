565114338863c90c9670035d636d9a4746416c6c
Taras Kushnir <taras.kushnir.tk@gmail.com>
2021-05-22 23:58:16 +0200

    Dodano KalkilatorApplication

---
+++ src/test/java/com/homeWork/Kalkulator/KalkulatorApplicationTests.java
@@ -0,0 +1,23 @@
+package com.homeWork.Kalkulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalkulatorApplicationTests {
+    @Autowired
+    @Test
+    void min() {
+    }
+
+    @Autowired
+    @Test
+    void max() {
+    }
+    @Autowired
+    @Test
+    void isPositive() {
+    }
+
+}
